9ddfab737d6b3c9bdacd333d5de8589f1b594fc2
Ntwali-Wilson <166679506+Ntwali-Wilson@users.noreply.github.com>
2026-01-29 11:01:14 -0800

    Serve HTML responses for home and about endpoints
    Refactor home endpoint to serve HTML using Jinja2 templates and add about endpoint.

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import os
+from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "running", "environment": "Render"}
+# Tell FastAPI to look in the "templates" folder
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def serve_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def serve_about(request: Request):
+    # We can pass variables (like time) to the HTML template
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return templates.TemplateResponse("about.html", {"request": request, "server_time": now})
 
 if __name__ == "__main__":
-    # Render provides a $PORT environment variable
     port = int(os.environ.get("PORT", 10000))
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
